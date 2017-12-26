863929eff966e00fa80e701369f3015ca5be3d21
Megan <megareceiver@gmail.com>
2017-12-27 03:16:20 +0700

    up

--- assets/js/partial.js
+++ assets/js/partial.js
@@ -20,6 +20,24 @@ $(function(){
 			$(elem).removeClass(act);
 		}
 	});
+
+	/*tawk to*/
+	var tawk =
+	'<!--Start of Tawk.to Script-->' +
+	'<script type="text/javascript">' +
+	'var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();' +
+	'(function(){' +
+	'var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];' +
+	's1.async=true;' +
+	's1.src="https://embed.tawk.to/5a377882f4461b0b4ef89517/default";' +
+	's1.charset="UTF-8";' +
+	's1.setAttribute("crossorigin","*");' +
+	's0.parentNode.insertBefore(s1,s0);' +
+	'})();' +
+	'</script>' +
+	'<!--End of Tawk.to Script-->';
+
+	$("body").append(tawk);
 });
 
 function r_setCookie(cname,cvalue,exdays="1") {

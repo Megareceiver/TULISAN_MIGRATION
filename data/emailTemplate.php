3bf5154a3ea01c3238c12b1fea31e126299c4ced
Megan <megareceiver@gmail.com>
2018-01-07 12:09:36 +0700

    up

---
+++ data/emailTemplate.php
@@ -0,0 +1,430 @@
+<?php
+  $path = "../assets";
+?>
+
+<section style="float:left; width:100%;max-width:700px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; font-size: 11px">
+  <img src="<?=$path?>/PICS/tulisan.png" style="width: 200px" />
+  <span style="float: right; color: #CCC">INVOICE</span>
+  <hr style="margin: 20px 0; "/>
+  <h3>Thank you for your order!</h3>
+  <p>Your order number is#</p>
+  <hr style="margin: 20px 0 0 0; "/>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Shipping Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Billing Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <table width="100%" border="0" style="border-collapse: separate; margin-top: 30px; font-size: 11px" >
+    <tr style="background-color: #E9E2DB; height: 40px">
+      <th align="left" style="padding: 0 10px">Item</th>
+      <th>SKU</th>
+      <th>Price</th>
+      <th>Discount</th>
+      <th>Qty</th>
+      <th>Total</th>
+    </tr>
+    <tr style="background-color: #EEEEEE; height: 40px">
+      <td width="30%" style="padding: 0 10px">Item</td>
+      <td align="center" style="padding: 0 10px">SKU</td>
+      <td align="right" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Cart Total</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Discount</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Sales tax (TAX)</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Shipping</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5"><b>Grand Total</b></td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+  </table>
+  <hr style="margin: 20px 0; "/>
+  <p>
+    Please read the important information below thoroughly. <br/><br/>
+
+    Dear our beloved customers, <br/><br/>
+
+    We now have a new payment method with DOKU. Since the new instalment is still relatively new and we are doing the process manually, we need your email confirming you selected DOKU as your payment method. Please simply reply this order confirmation to us and wait for our email to process your payment for your order.
+    Orders placed on Fridays, Saturdays, Sundays or holidays will be processed the following business day. We will ship your order, after we receiver your payment confirmation from DOKU automated system.
+    Please note that your order will automatically be cancelled if the payment is not confirmed within 24 hours after the order is made. You will be notified when your payment has been verified by our team.
+    If you select payment via Bank Transfer, please confirm your payment by sending the proof of payment (scanned or picture image) directly to our email (shop@tulisan.com) or simply reply back our Order Confirmation email and we will process and ship your immediately after we receive the proof.
+
+    <br/><br/>
+    Please make payment transfer to:
+    <br/><br/>
+    PT. Tulisan Susunan Tinta<br/>
+    Bank Mandiri<br/>
+    Branch: Iskandarsyah (Jakarta-Selatan)<br/>
+    Acc. No. 126-00-0689973-5<br/>
+    <br/><br/>
+    (Rupiah Account)
+    <br/><br/>
+    After making your transfer, please verify it by sending the proof of payment to shop@tulisan.com (you can scan or take a picture of it) with your order number and we will process your order after we receive it. Please note that shipping maybe delayed if we need to verify your information with you or your payment process.
+    Please allow 1-2 business days to process your order from our warehouse. Our standard shipping (free delivery) typically arrives between 3-5 business days. Rural and remote areas may incur longer delivery times. Orders placed on Fridays, Saturdays, Sundays or holidays will ship the following business day after we receive your proof of payment.
+    Note that your order will automatically be cancelled if the payment is not confirmed within 24 hours after the order is made. You will be notified when your payment has been verified by our team. Orders are only shipped after we receive your full payment.
+    <br/><br/>
+    Warmest regards,
+    <br/><br/><br/>
+    Team Tulisan.
+    <br/><br/>
+
+
+    <hr style="margin: 20px 0; "/>
+    Jika anda memilih metode pembayaran dengan DOKU, mohon tunggu email dari kami untuk dapat memproses pembayaran untuk pesanan Anda. Pesanan yang masuk ke sistem kami pada hari Jumat, Sabtu, minggu atau hari libur akan dikirim pada harı kerja berikutnya. Setelah kami Sudah memverifikasi pembayaran Anda. Kami akan memproses dan mengirimkan pesanan Anda, setelah kami menerima konfirmasi pembayaran dari Sistem Automasi DOKU dari pembayaran yang telah dilakukan.
+    Order Anda akan secara otomatis dibatalkan apabila pembayaran tidak terkonfirmasi dalam 24 jam setelah pembelin dilakukan. Anda akan menerima notifikasi saat pembayaran Anda telah diverifikasi Oleh Tim Tulisan.
+    Jika Anda memilih Pembayaran via bank transfer, mohon transfer ke:
+    <br/><br/>
+    PT. Tulisan Susunan Tinta<br/>
+    Bank Mandiri<br/>
+    Cabang: Iskandarsyah (Jakarta-Selatan)<br/>
+    Acc. No. 126-00-0689973-5<br/>
+    <br/><br/>
+    (Rupiah Account)
+    <br/><br/>
+    Setelah melakukan pembayaran, verifikasi status pembayaran Anda tengan mengirimkan bukti pembayaran ke shop@tulisan.com (bisa berupa scan atau foto) dengan memberikan nommer order pesanan Anda dan kami akan memproses pesanan Anda setelah kami menerima buktinya. Untuk diketahui pengiriman mungkin akan terlambat jika kami harus memverifikasi informasi kepada Anda atau tentang proses pembayaran Anda.
+    Kami membutuhkan 1-2 hari uituk memproses pesanan Anda dari warehouse kami. Pesanan yang masuk ke sistem kami pada hari Jumat, Sabtu, mingau atau hari libar akan dikirimkan pada hari kerja berikutnya setelah kami Sudan memverifikasi pembayaran Anda.
+    Order Anda akan secara otomatis dibatalkan apabila pembayaran tidak terkonfirmasi dalam 24 jam setelah pembelian dilakukan. Lahanan jasa pengiriman reguler kami (gratis biara pengiriman) biasanya sampai ditujuan antara 3-5 hari kerja. Untuk daerah jauh dari pusat pengiriman kami (yaitu Jakarta) akan sampai di tujuan lebih lama dari biasanya. Anda akan menerima notifikasi saat pembayaran Anda telah diverifikasi oleh Tim Tulisan. Order akan dikirimkan setelah pembayaran telah dilakukan secara penuh.
+    <br/><br/>
+    Salam hangat,
+    <br/><br/><br/>
+    Tim Tulisan.
+  </p>
+  <div style="margin-top: 20px">
+    <h3>PT TULISAN SUSUNAN TINTA</h3>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>Darmawangsa Square – The City Walk </p>
+      <p>Ground Floor | Unit 24</p>
+      <p>Jalan Darmawangsa VI</p>
+      <p>Jakarta 12160, Indonesia</p>
+    </div>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>T +62 21 7278 0235</p>
+      <p>shop@tulisan.com</p>
+      <p>www.tulisan.com</p>
+    </div>
+  </div>
+</section>
+
+<section style="float:left; width:100%;max-width:700px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; font-size: 11px">
+  <img src="<?=$path?>/PICS/tulisan.png" style="width: 200px" />
+  <hr style="margin: 20px 0; "/>
+  <h3>Congratulations, your order is being processed!</h3>
+  <p>Your order number is <b>#</b>, Created on <b></b></p>
+  <p>Subtotal: <b>0</b></p>
+  <p>Shipping: <b>0</b></p>
+  <p>Total: <b>0</b></p>
+  <table width="100%" border="0" style="border-collapse: separate; margin-top: 10px; font-size: 11px" >
+    <tr style="background-color: #E9E2DB; height: 40px">
+      <th align="left" style="padding: 0 10px">Item</th>
+      <th>SKU</th>
+      <th>Price</th>
+      <th>Discount</th>
+      <th>Qty</th>
+      <th>Total</th>
+    </tr>
+    <tr style="background-color: #EEEEEE; height: 40px">
+      <td width="30%" style="padding: 0 10px">Item</td>
+      <td align="center" style="padding: 0 10px">SKU</td>
+      <td align="right" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Cart Total</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Discount</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Sales tax (TAX)</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Shipping</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5"><b>Grand Total</b></td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+  </table>
+  <hr style="margin: 20px 0 0 0; "/>
+  <h3>Customer details</h3>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Shipping Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Billing Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="clear: both"></div>
+  <hr style="margin: 20px 0 0 0; "/>
+  <p>This is an automatic message, Do not reply to it.</p>
+  <div style="margin-top: 20px">
+    <h3>PT TULISAN SUSUNAN TINTA</h3>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>Darmawangsa Square – The City Walk </p>
+      <p>Ground Floor | Unit 24</p>
+      <p>Jalan Darmawangsa VI</p>
+      <p>Jakarta 12160, Indonesia</p>
+    </div>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>T +62 21 7278 0235</p>
+      <p>shop@tulisan.com</p>
+      <p>www.tulisan.com</p>
+    </div>
+  </div>
+</section>
+
+<section style="float:left; width:100%;max-width:700px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; font-size: 11px">
+  <img src="<?=$path?>/PICS/tulisan.png" style="width: 200px" />
+  <hr style="margin: 20px 0; "/>
+  <h3>Great News, your order has shipped!</h3>
+  <p>Your order number is <b>#</b>, Created on <b></b></p>
+  <hr style="margin: 10px 0; "/>
+  <p>Your tracking number is <b></b></p>
+  <p>Free delivery within Indonesia: Your tracking number is (nomor airway bill / nomor resi) please</p>
+  <p>Check it here : <b></b></p>
+  <hr style="margin: 10px 0; "/>
+  <div style="display: inline-block; width: 49%;">
+    <h4>Shipping Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="clear: both"></div>
+  <table width="100%" border="0" style="border-collapse: separate; margin-top: 10px; font-size: 11px" >
+    <tr style="background-color: #E9E2DB; height: 40px">
+      <th align="left" style="padding: 0 10px">Item</th>
+      <th>SKU</th>
+      <th>Price</th>
+      <th>Discount</th>
+      <th>Qty</th>
+      <th>Total</th>
+    </tr>
+    <tr style="background-color: #EEEEEE; height: 40px">
+      <td width="30%" style="padding: 0 10px">Item</td>
+      <td align="center" style="padding: 0 10px">SKU</td>
+      <td align="right" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Cart Total</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Discount</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Sales tax (TAX)</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Shipping</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5"><b>Grand Total</b></td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+  </table>
+  <hr style="margin: 20px 0 0 0; "/>
+  <p>This is an automatic message, Do not reply to it.</p>
+  <div style="margin-top: 20px">
+    <h3>PT TULISAN SUSUNAN TINTA</h3>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>Darmawangsa Square – The City Walk </p>
+      <p>Ground Floor | Unit 24</p>
+      <p>Jalan Darmawangsa VI</p>
+      <p>Jakarta 12160, Indonesia</p>
+    </div>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>T +62 21 7278 0235</p>
+      <p>shop@tulisan.com</p>
+      <p>www.tulisan.com</p>
+    </div>
+  </div>
+</section>
+
+<section style="float:left; width:100%;max-width:700px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; font-size: 11px">
+  <img src="<?=$path?>/PICS/tulisan.png" style="width: 200px" />
+  <hr style="margin: 20px 0; "/>
+  <h3>Congratulations, your order has completed!</h3>
+  <h2>Thank you, we are waiting for your next order.</h2>
+  <p>Your order number is <b>#</b>, Created on <b></b></p>
+  <p>Subtotal: <b>0</b></p>
+  <p>Shipping: <b>0</b></p>
+  <p>Total: <b>0</b></p>
+  <table width="100%" border="0" style="border-collapse: separate; margin-top: 10px; font-size: 11px" >
+    <tr style="background-color: #E9E2DB; height: 40px">
+      <th align="left" style="padding: 0 10px">Item</th>
+      <th>SKU</th>
+      <th>Price</th>
+      <th>Discount</th>
+      <th>Qty</th>
+      <th>Total</th>
+    </tr>
+    <tr style="background-color: #EEEEEE; height: 40px">
+      <td width="30%" style="padding: 0 10px">Item</td>
+      <td align="center" style="padding: 0 10px">SKU</td>
+      <td align="right" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Cart Total</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Discount</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Sales tax (TAX)</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Shipping</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5"><b>Grand Total</b></td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+  </table>
+  <hr style="margin: 20px 0 0 0; "/>
+  <h3>Customer details</h3>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Shipping Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Billing Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="clear: both"></div>
+  <hr style="margin: 20px 0 0 0; "/>
+  <p>This is an automatic message, Do not reply to it.</p>
+  <div style="margin-top: 20px">
+    <h3>PT TULISAN SUSUNAN TINTA</h3>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>Darmawangsa Square – The City Walk </p>
+      <p>Ground Floor | Unit 24</p>
+      <p>Jalan Darmawangsa VI</p>
+      <p>Jakarta 12160, Indonesia</p>
+    </div>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>T +62 21 7278 0235</p>
+      <p>shop@tulisan.com</p>
+      <p>www.tulisan.com</p>
+    </div>
+  </div>
+</section>
+
+<section style="float:left; width:100%;max-width:700px; font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; font-size: 11px">
+  <img src="<?=$path?>/PICS/tulisan.png" style="width: 200px" />
+  <hr style="margin: 20px 0; "/>
+  <h3>Your order has been canceled!</h3>
+  <h2>Thank you, we are waiting for your next order.</h2>
+  <p>Your recent order number is <b>#</b>, Created on <b></b></p>
+  <p>Subtotal: <b>0</b></p>
+  <p>Shipping: <b>0</b></p>
+  <p>Total: <b>0</b></p>
+  <table width="100%" border="0" style="border-collapse: separate; margin-top: 10px; font-size: 11px" >
+    <tr style="background-color: #E9E2DB; height: 40px">
+      <th align="left" style="padding: 0 10px">Item</th>
+      <th>SKU</th>
+      <th>Price</th>
+      <th>Discount</th>
+      <th>Qty</th>
+      <th>Total</th>
+    </tr>
+    <tr style="background-color: #EEEEEE; height: 40px">
+      <td width="30%" style="padding: 0 10px">Item</td>
+      <td align="center" style="padding: 0 10px">SKU</td>
+      <td align="right" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="center" style="padding: 0 10px">0</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Cart Total</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Discount</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Sales tax (TAX)</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5">Shipping</td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+    <tr>
+      <td align="right" colspan="5"><b>Grand Total</b></td>
+      <td align="right" style="padding: 0 10px">0</td>
+    </tr>
+  </table>
+  <hr style="margin: 20px 0 0 0; "/>
+  <h3>Customer details</h3>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Shipping Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="float:left; display: inline-block; width: 49%;">
+    <h4>Billing Address</h4>
+      <p>Customer name</p>
+      <p>Address</p>
+  </div>
+  <div style="clear: both"></div>
+  <hr style="margin: 20px 0 0 0; "/>
+  <p>This is an automatic message, Do not reply to it.</p>
+  <div style="margin-top: 20px">
+    <h3>PT TULISAN SUSUNAN TINTA</h3>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>Darmawangsa Square – The City Walk </p>
+      <p>Ground Floor | Unit 24</p>
+      <p>Jalan Darmawangsa VI</p>
+      <p>Jakarta 12160, Indonesia</p>
+    </div>
+    <div style="float:left; display: inline-block; width: 49%;">
+      <p>T +62 21 7278 0235</p>
+      <p>shop@tulisan.com</p>
+      <p>www.tulisan.com</p>
+    </div>
+  </div>
+</section>
